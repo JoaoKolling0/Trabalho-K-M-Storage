d1bdf0fc01026287c820d53be27ab05891168e66
JoaoKolling0 <joao.victhor.kolling@gmail.com>
2025-07-25 14:46:50 -0300

    Atualizacao 02

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ class User:
         self.nome = nome
         self.status = status
         
-    def cadastro(self):
+    def cadastro(self, cadastrar_email, cadastrar_senha):
         if self.status == "cadastrado":
             return f"{self.nome} já é cadastrado"
+        else:
+            self.cadastrar_email = cadastrar_email
+            self.cadastrar_senha = cadastrar_senha
         
     def login(self, email, senha):
         self.email = email
